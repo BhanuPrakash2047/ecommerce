dd7a7b787dcbc38b41891e393424c07b9ea85ca2
BhanuPrakash2047 <168528750+BhanuPrakash2047@users.noreply.github.com>
2026-01-14 00:44:07 +0530

    Deleted and moved snacky-ui directory

--- snacky-ui/src/components/features/index.js
+++
@@ -1,4 +0,0 @@
-export { default as ProductCard } from './ProductCard';
-export { default as ProductGrid } from './ProductGrid';
-export { default as HeroSection } from './HeroSection';
-export { default as ReviewSection } from './ReviewSection';
